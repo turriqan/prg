68415b223d2264a0f06498ede6b0d140c742da43
turriqan <zaq@xs4all.nl>
2015-09-20 13:43:36 +0200

    console start

---
+++ console/exodus/exodus/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace exodus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Init();
+            Waitkey();
+        }
+
+        private static void Init()
+        {
+            LogContext("program start", ELevel.Informative);
+        }
+        private static void LogContext(string value, ELevel eLevel)
+        {
+            ConsoleColor color = Console.ForegroundColor;
+            switch (eLevel)
+            {
+                case ELevel.Informative:
+                    {
+                        color = ConsoleColor.Green;
+                        break;
+                    }
+                case ELevel.Warning:
+                    {
+                        color = ConsoleColor.Yellow;
+                        break;
+                    }
+                case ELevel.Error:
+                    {
+                        color = ConsoleColor.Red;
+                        break;
+                    }
+                default:
+                    {
+                        color = ConsoleColor.Gray;
+                        break;
+                    }
+            }
+            Console.ForegroundColor = color;
+            Console.WriteLine(String.Format("exodus: {0}", value));
+        }
+
+
+        private static void Waitkey()
+        {
+            Console.ReadKey();
+        }
+        private enum ELevel
+        {
+            Normal,
+            Informative,
+            Warning,
+            Error
+        }
+    }
+}
